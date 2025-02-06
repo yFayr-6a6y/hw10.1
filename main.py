3c46ffe2cf9c7f1a81c4b7e5bb8165654c630025
Yaroslav Ugolnikov <kakanop7xiaomi@gmail.com>
2025-02-06 13:07:58 +0300

    new

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.widget import mask_account_card, get_date
+
+if __name__ == "__main__":
+    mask_card_number = mask_account_card("Visa Platinum 7000792289606361")
+    mask_account_number = get_date("2024-03-11T02:26:18.671407")
+    print(mask_card_number)
+    print(mask_account_number)
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,10 @@
-from tests.test import get_mask_card_number
 
 
-def mask_account_card(card: str) -> str:
+from src.masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+
+def mask_account_card(card: str | int) -> str | int:
     new_list = card.split(" ")
     card_num = []
     card_alph = []
@@ -12,9 +15,21 @@ def mask_account_card(card: str) -> str:
         else:
             card_num.append(int(i))
 
-    a = card_alph
-    b = get_mask_card_number(card_num)
+    name_letter_card = ' '.join(card_alph)
 
-    return f"{a}{b}"
 
-print(mask_account_card())ff
+    if name_letter_card == 'Счет':
+        name_number_card = get_mask_account(''. join(card_num))
+    else:
+        name_number_card = get_mask_card_number (''.join(card_num))
+
+
+    return f'{name_letter_card} {name_number_card}'
+
+def get_date(my_date: str) -> str:
+    date_object = datetime.strptime(my_date, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_object. strftime("2024-03-11T02:26:18.67140")
+
+
+
+print(get_date("2024-03-11T02:26:18.67140"))
