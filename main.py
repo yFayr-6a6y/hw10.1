2c408f06f24a884c8a9d2cefc489acc8e3ce9145
Yaroslav Ugolnikov <kakanop7xiaomi@gmail.com>
2025-02-06 15:38:50 +0300

    тт

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
     mask_card_number = mask_account_card("Visa Platinum 7000792289606361")
