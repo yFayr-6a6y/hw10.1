68dfb90fbb27af34045dc74031206d1e2865c911
Yaroslav Ugolnikov <kakanop7xiaomi@gmail.com>
2025-02-11 14:53:24 +0300

    переделанное

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
-def filter_by_state(data, state='EXECUTED'):
+def filter_by_state(data, state="EXECUTED"):
     """
     фильтрует список словарей по значению ключа 'state'.
     """
-    return [item for item in data if item.get('state') == state]
+    return [item for item in data if item.get("state") == state]
 
 
 def sort_by_date(data, descending=True):
     """
     сортирует список словарей по ключу 'date'.
     """
-    return sorted(data, key=lambda x: x.get('date'), reverse=descending)
+    return sorted(data, key=lambda x: x.get("date"), reverse=descending)
--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,18 @@
-def mask_account_card(card_info):
-    import re
+from typing import Optional, Tuple
+import re
+from datetime import datetime
 
-    def mask_card_number(number):
-        return number[:4] + ' ' + number[4:6] + '** **** ' + number[-4:]
 
-    def mask_account_number(number):
-        return '**' + number[-4:]
+def mask_account_card(card_info: str) -> str:
+    """основная функция с подфункциями"""
+
+    def mask_card_number(number: str) -> str:
+        """функция маскировки карты"""
+        return number[:4] + " " + number[4:6] + "** **** " + number[-4:]
+
+    def mask_account_number(number: str) -> str:
+        """тоже маскиоровка карты"""
+        return "**" + number[-4:]
 
     card_pattern = re.compile(r"(Visa|Maestro) (\d{16})")
     account_pattern = re.compile(r"(Счет) (\d{20})")
@@ -19,7 +26,10 @@ def mask_account_card(card_info):
     else:
         return card_info
 
-def get_date(date_str):
+
+def get_date(date_str: str) -> str:
+    """функция для даты"""
     from datetime import datetime
+
     date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
     return date_obj.strftime("%d.%m.%Y")
