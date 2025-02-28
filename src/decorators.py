178bda5a9dee75fbaf84cb7d37c50c187bc6e01c
Yaroslav Ugolnikov <kakanop7xiaomi@gmail.com>
2025-02-28 13:59:41 +0300

    11.2

--- src/decorators.py
+++ src/decorators.py
@@ -2,9 +2,40 @@ import logging
 import functools
 
 def log(filename=None):
+    """
+      Декоратор для логирования вызовов функций, их результатов и возникающих исключений.
+
+      Args:
+          filename (str, optional): Имя файла для записи логов. Если не указано, логи выводятся в консоль.
+
+      Returns:
+          callable: Декоратор, который оборачивает функцию и добавляет логирование.
+      """
     def decorator(func):
+        """
+               Внутренний декоратор, выполняющий логирование.
+
+               Args:
+                   func (callable): Функция, которую необходимо залогировать.
+
+               Returns:
+                   callable: Обернутая функция с добавленным логированием.
+               """
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            """
+                        Обертка для функции, выполняющая логирование до и после ее вызова.
+
+                        Args:
+                            *args: Позиционные аргументы функции.
+                            **kwargs: Именованные аргументы функции.
+
+                        Returns:
+                            любой: Результат вызова функции.
+
+                        Raises:
+                            Exception: Если в функции происходит исключение, оно логируется и перевыбрасывается.
+                        """
             func_name = func.__name__
             logging.basicConfig(filename=filename, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
             logging.info(f"Функция '{func_name}' начата")
